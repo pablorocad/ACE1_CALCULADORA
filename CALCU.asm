615f53523d2d9c49cbda058f9e1cb012fe787402
lKonse <lpablord28@gmail.com>
2020-03-30 15:28:00 -0600

    Funcion normal

--- CALCU.asm
+++ CALCU.asm
@@ -8,15 +8,22 @@ inicio db 0ah,0dh,'UNIVERSIDAD DE SAN CARLOS DE GUATEMALA',0ah,0dh,'FACULTAD DE 
 menuTexto db 0ah,0dh,'1) Ingresar funcion f(x)',0ah,0dh,'2) Funcion en memoria',0ah,0dh,'3) Derivada f',27h,'(x)',0ah,0dh,'4) Integral F(x)',0ah,0dh,'5) Graficar funciones',0ah,0dh,'6) Reporte',0ah,0dh,'7) Modo calculadora',0ah,0dh,'8) Salir',0ah,0dh,'opcion: ', '$'
 ;=============INGRESAR FUNCION============
 msgCoeficiente db 0ah,0dh,'Coeficiente de x',0,': ','$'
-minus db '-','$'
-max db '+','$'
+minus db ' - ','$'
+max db ' + ','$'
 numSig db 1
 numTemp db 0
 ;============Mostrar funciones=====
-msgFun db 0ah,0dh,'f(x) = ','$'
+msgFx db 0ah,0dh,'f(x) = ','$'
+msgFderx db 0ah,0dh,'f',27h,'(x) = ','$'
+msgFintx db 0ah,0dh,'F(x) = ','$'
+val4 db '*x4','$'
+val3 db '*x3','$'
+val2 db '*x2','$'
+val1 db '*x','$'
+numero_a_imprimir db 0
 ;=============ARREGLOS============
 ;Funcion normal
-fun db 0,0,0,0,0,'$'
+fun db 0,0,0,0,0
 ;Funcion derivada
 fun_der db 0,0,0,0
 ;Funcion integrada
@@ -110,7 +117,7 @@ jmp menuPrincipal
 ;=========================MOSTRAR =FUNCION===========================
 ;====================================================================
 funcionEnMemoria:
-printNum fun[3]
+call mostrar_funcionEnMemoria
 jmp menuPrincipal
 
 ;====================================================================
@@ -156,7 +163,104 @@ int 21h
 ret
 getchar endp
 
+mostrar_funcionEnMemoria proc near
+pushear
 
+print msgFx
+
+mov ah,fun[4]
+cmp ah,0
+je v3
+mov numero_a_imprimir,ah
+call printNum
+print val4
+
+v3:
+mov ah,fun[3]
+cmp ah,0
+je v2
+mov numero_a_imprimir,ah
+call printNum
+print val3
+
+v2:
+mov ah,fun[2]
+cmp ah,0
+je v1
+mov numero_a_imprimir,ah
+call printNum
+print val2
+
+v1:
+mov ah,fun[1]
+cmp ah,0
+je v0
+mov numero_a_imprimir,ah
+call printNum
+print val1
+
+v0:
+mov ah,fun[0]
+cmp ah,0
+je fin_proceso
+mov numero_a_imprimir,ah
+call printNum
+
+fin_proceso:
+popear
+ret
+mostrar_funcionEnMemoria endp
+
+printNum proc near
+pushear
+
+mov ax,@data
+mov ds, ax;LIMPIAMOS
+
+mov al,numero_a_imprimir
+
+;=============Comparamos si es negativa o positiva================
+mov bl,-1
+cmp al,1
+jg _imprimir_pos;=====Si es positiva no hacemos ningun cambio========
+imul bl;=Si es negativa convertimos a positiva e imprimimos signo======
+print minus
+jmp _imprimir_num
+
+_imprimir_pos:
+print max
+
+_imprimir_num:
+xor bx,bx
+
+;mov al,p1
+aam
+mov bx,ax
+
+;===========decenas
+cmp bh,0
+je unidades
+
+decenas:
+mov ah,02h
+mov dl,bh
+add dl,30h
+int 21h
+
+;==========unidades
+unidades:
+cmp bl,0
+je fin
+
+mov ah,02h
+mov dl,bl
+add dl,30h
+int 21h
+
+fin:
+popear
+ret
+printNum endp
 ;====================================================================
 ;=================================FINAL==============================
 ;====================================================================
