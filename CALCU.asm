97235e8fea782417c5b3711711110f726c4ae4e7
lKonse <lpablord28@gmail.com>
2020-03-30 14:05:23 -0600

    GUardar datos funcion

--- CALCU.asm
+++ CALCU.asm
@@ -6,7 +6,21 @@ include macro.asm
 
 inicio db 0ah,0dh,'UNIVERSIDAD DE SAN CARLOS DE GUATEMALA',0ah,0dh,'FACULTAD DE INGENIERIA',0ah,0dh,'ESCUELA CIENCIAS Y SISTEMAS',0ah,0dh,'ARQUITECTURA DE COMPUTADORES Y ENSAMBLADORES 1 A',0ah,0dh,'PRIMER SEMESTRE 2020',0ah,0dh,'PABLO ANDR',90h,'S ROCA DOMINGUEZ',0ah,0dh,'CARNET: 201700584',0ah,0dh,'QUINTA PRACTICA',0ah,0dh,'$' ;caracter "$" para finalizar cadenas;
 menuTexto db 0ah,0dh,'1) Ingresar funcion f(x)',0ah,0dh,'2) Funcion en memoria',0ah,0dh,'3) Derivada f',27h,'(x)',0ah,0dh,'4) Integral F(x)',0ah,0dh,'5) Graficar funciones',0ah,0dh,'6) Reporte',0ah,0dh,'7) Modo calculadora',0ah,0dh,'8) Salir',0ah,0dh,'opcion: ', '$'
-
+;=============INGRESAR FUNCION============
+msgCoeficiente db 0ah,0dh,'Coeficiente de x',0,': ','$'
+minus db '-','$'
+max db '+','$'
+numSig db 1
+numTemp db 0
+;============Mostrar funciones=====
+msgFun db 0ah,0dh,'f(x) = ','$'
+;=============ARREGLOS============
+;Funcion normal
+fun db 0,0,0,0,0,'$'
+;Funcion derivada
+fun_der db 0,0,0,0
+;Funcion integrada
+fun_int db 0,0,0,0,0
 
 .code
 ;====================================================================
@@ -36,24 +50,96 @@ cmp al,37h
 cmp al,38h
   je salir
 
+  ;====================================================================
+  ;=========================INGRESAR FUNCION===========================
+  ;====================================================================
 ingresarFuncion:
+pushear
+
+xor bx,bx;limpiamos bx
+mov cx,5
+mov bx,4
+bucle_ingresarFunc:
+
+push bx
+add bl,30h
+mov msgCoeficiente[18],bl
+print msgCoeficiente
+pop bx
+
+call getchar
+
+;==========VEMOS SI HAY SIGNO Y CUAL==========
+_neg:
+cmp al,2dh;'-'
+jne _pos
+mov numSig,-1
+jmp _coeficiente
+
+_pos:
+cmp al,2bh;'+'
+jne _entero
+mov numSig,1
+jmp _coeficiente
+
+_entero:;====SI SOLO VIENE NUMERO====
+sub al,30h
+mov fun[bx],al
+;printNum fun[bx]
+jmp _fin_bucle_ingresarFunc
+
+_coeficiente:;====AQUI PREGUNTA EL COEFICIENTE====
+call getchar
+push bx
+mov bl,numSig
+sub al,30h
+imul bl
+pop bx
+mov fun[bx],al
+
+_fin_bucle_ingresarFunc:
+dec bx
+dec cx
+cmp cx,0
+jne bucle_ingresarFunc
+
+popear
 jmp menuPrincipal
 
+;====================================================================
+;=========================MOSTRAR =FUNCION===========================
+;====================================================================
 funcionEnMemoria:
+printNum fun[3]
 jmp menuPrincipal
 
+;====================================================================
+;=========================MOSTRAR DERIVADA===========================
+;====================================================================
 mostrarDerivada:
 jmp menuPrincipal
 
+;====================================================================
+;=========================MOSTRAR INTEGRAL===========================
+;====================================================================
 mostrarIntegral:
 jmp menuPrincipal
 
+;====================================================================
+;============================GRAFICAR================================
+;====================================================================
 graficar:
 jmp menuPrincipal
 
+;====================================================================
+;=============================REPORTE================================
+;====================================================================
 reporte:
 jmp menuPrincipal
 
+;====================================================================
+;============================CALCULADORA=============================
+;====================================================================
 modoCalculadora:
 jmp menuPrincipal
 
--- macro.asm
+++ macro.asm
@@ -8,6 +8,57 @@ int 21h
 popear           ;Interruptions DOS Functions
 endm
 
+printNum macro p1
+pushear
+xor bx,bx
+
+mov ax,@data
+mov ds, ax;LIMPIAMOS
+
+mov al,p1
+
+;=============Comparamos si es negativa o positiva================
+mov bl,-1
+cmp al,1
+jg _imprimir_pos;=====Si es positiva no hacemos ningun cambio========
+imul bl;=Si es negativa convertimos a positiva e imprimimos signo======
+print minus
+jmp _imprimir_num
+
+_imprimir_pos:
+print max
+
+_imprimir_num:
+xor bx,bx
+
+;mov al,p1
+aam
+mov bx,ax
+
+;===========decenas
+cmp bh,0
+je unidades
+
+decenas:
+mov ah,02h
+mov dl,bh
+add dl,30h
+int 21h
+
+;==========unidades
+unidades:
+cmp bl,0
+je fin
+
+mov ah,02h
+mov dl,bl
+add dl,30h
+int 21h
+
+fin:
+popear
+endm
+
 pushear macro
   push ax
   push bx
